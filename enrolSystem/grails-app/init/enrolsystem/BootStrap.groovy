43d099421cfb9cea9dbb56ecc1f92dbaa2d87ff7
Maxw78 <maxwallen78@gmail.com>
2020-02-09 17:53:45 +0000

    Added Bootstrap Data for Courses

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -3,6 +3,13 @@ package enrolsystem
 class BootStrap {
 
     def init = { servletContext ->
+
+	new Course(department:'Computing', courseTitle:'BSc Hon Computing', courseLeader:'Dr Michelle Murphy', courseCode:'CS123', startDate:new Date('09/09/2019'), endDate:new Date('07/07/2023'), studyMode:'Fulltime', description:'Example Description', numberOfStudents:'55', tuitionFees:9000.60).save()
+
+new Course(department:'Geology', courseTitle:'BSc Hon Geology', courseLeader:'Dr Smith Smith', courseCode:'GG123', startDate:new Date('09/09/2019'), endDate:new Date('07/07/2023'), studyMode:'Fulltime', description:'Example Description', numberOfStudents:'35', tuitionFees:9000.50).save()
+
+new Course(department:'Smithing', courseTitle:'BSc Hon Smithing', courseLeader:'Mrs Dark Green', courseCode:'SM123', startDate:new Date('09/09/2019'), endDate:new Date('07/07/2023'), studyMode:'PartTime', description:'Example Description', numberOfStudents:'23', tuitionFees:9000.50).save()
+
     }
     def destroy = {
     }
