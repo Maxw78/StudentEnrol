df54055470711326813f488ad8e6569bb506a9f0
Maxw78 <maxwallen78@gmail.com>
2020-02-09 16:02:18 +0000

    Added student domain, class and view.

---
+++ enrolSystem/grails-app/domain/enrolsystem/Student.groovy
@@ -0,0 +1,24 @@
+package enrolsystem
+
+class Student {
+
+String studentName
+	String studentID
+	Date dob
+	Boolean isFundingAvailable
+	String studentEmail
+	String studentUsername
+	String studentPassword
+	String course
+
+    static constraints = {
+	studentID blank: false, nullable: false
+	dob blank: false, nullable: false
+	isFundingAvailable blank: false, nullable: false
+	studentEmail blank: false, nullable: false, email: true
+	studentUsername blank: false, nullable: false, unique: true
+	studentPassword blank: false, nullable: false
+	course blank: false, nullable: false
+
+    }
+}
